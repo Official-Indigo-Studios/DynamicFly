985c7739ad3bdc2f32749db72750e36a58f4c46f
thebirmanator <birman.girl@att.net>
2020-03-21 09:21:40 -0700

    Fixed claim checks not checking when player teleports

--- src/main/java/ltd/indigostudios/dynamicfly/listeners/PlayerMoveListener.java
+++ src/main/java/ltd/indigostudios/dynamicfly/listeners/PlayerMoveListener.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.util.HashSet;
@@ -22,8 +23,15 @@ public class PlayerMoveListener implements Listener {
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        Location from = event.getFrom();
-        Location to = event.getTo();
+        evaluate(event.getPlayer(), event.getTo(), event.getFrom());
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        evaluate(event.getPlayer(), event.getTo(), event.getFrom());
+    }
+
+    private void evaluate(Player player, Location to, Location from) {
         // they just rotated, no need to check
         if (from.getX() == to.getX() && from.getY() == to.getY() && from.getZ() == to.getZ()) return;
 
@@ -35,16 +43,16 @@ public class PlayerMoveListener implements Listener {
                 Object toClaim = hook.getGenericClaimAt(to);
                 // one claim or zero claims can be null (but not both)
                 if (!(fromClaim == null && toClaim == null)) {
-                    Bukkit.getPluginManager().callEvent(new ClaimChangeEvent(event.getPlayer(), plugin, from, to));
+                    Bukkit.getPluginManager().callEvent(new ClaimChangeEvent(player, plugin, from, to));
                     // one claim is null and the other is not
                     if ((fromClaim == null || toClaim == null)) {
                         if (fromClaim == null) {
-                            Bukkit.getPluginManager().callEvent(new ClaimEnterEvent(event.getPlayer(), plugin, from, to));
+                            Bukkit.getPluginManager().callEvent(new ClaimEnterEvent(player, plugin, from, to));
                         } else {
-                            Bukkit.getPluginManager().callEvent(new ClaimLeaveEvent(event.getPlayer(), plugin, from, to));
+                            Bukkit.getPluginManager().callEvent(new ClaimLeaveEvent(player, plugin, from, to));
                         }
                     } else if (!fromClaim.equals(toClaim)) { // neither are null and they are different claims
-                        Bukkit.getPluginManager().callEvent(new ClaimTransitionEvent(event.getPlayer(), plugin, from, to));
+                        Bukkit.getPluginManager().callEvent(new ClaimTransitionEvent(player, plugin, from, to));
                     }
                 }
             }
